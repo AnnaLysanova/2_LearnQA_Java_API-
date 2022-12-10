338cf2d2d296dedb90517bfd00fe67eb1996aa68
AnnaLysanova <nyanechka@list.ru>
2022-12-10 13:19:27 +0300

    HelloWorldTest

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+        @Test
+        public void testHelloWorld(){
+            Response response = RestAssured
+                    .get("https://playground.learnqa.ru/api/hello")
+                    .andReturn();
+            response.prettyPrint();
+        }
+}
+
